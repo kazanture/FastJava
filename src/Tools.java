ebf33ecc3dc58e093f5a6f1b326054030c481b50
VirtualWorldsOnline <endemre@yahoo.com>
2016-10-02 20:18:33 +0300

    primat versions of some prime functions added

--- src/Tools.java
+++ src/Tools.java
@@ -1,33 +1,53 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ebayram on 1.10.2016.
  */
 public class Tools {
-    List<BigInteger> primeNumbers = new ArrayList<>();
-    BigInteger getNthPrime(int n) {
-        BigInteger biggestPrime = primeNumbers.get(primeNumbers.size() - 1);
-        BigInteger candidate = biggestPrime.add(BigInteger.ONE);
-        while (primeNumbers.size() < n) {
-
-            if (isPrime(candidate)) {
-                primeNumbers.add(candidate);
-            }
-            candidate = candidate.add(BigInteger.ONE);
+    Map<Long,Long> divisorCountMap=new HashMap<>();
+    long getDivisorCount(long n){
+        if(n==1)
+        {
+            return 1;
         }
-        return primeNumbers.get(n-1);
+        if(isPrime(n)){
+            return 2;
+        }
+        Long divisorCount=divisorCountMap.get(n);
+        if(divisorCount==null) {
+            divisorCount=new Long(1);
+            int index=1;
+            long nClone=n;
+            while(nClone!=1) {
+                long prime = getNthPrime(index);
 
+                long currentCount = 0;
+                while (nClone % prime == 0) {
+                    nClone = nClone / prime;
+                    currentCount++;
+                }
+                divisorCount*=currentCount+1;
+                index++;
+            }
+            divisorCountMap.put(n,divisorCount);
+        }
+
+        return  divisorCount;
     }
-    boolean isPrime(BigInteger primeCandidate) {
-        if (!primeCandidate.isProbablePrime(2)) {
+    boolean isPrime(long primeCandidate) {
+        BigInteger candidate=BigInteger.valueOf(primeCandidate);
+        if (!candidate.isProbablePrime(3)) {
             return false;
         } else {
             int n = 1;
-            BigInteger nthPrime = getNthPrime(n++);
-            while (nthPrime.compareTo(primeCandidate.divide(BigInteger.valueOf(2))) == -1) {
-                if (primeCandidate.mod(nthPrime).compareTo(BigInteger.valueOf(0)) == 0) {
+            long nthPrime = getNthPrime(n++);
+            long sqrt=(long)Math.sqrt(primeCandidate);
+            while (nthPrime<sqrt) {
+                if (primeCandidate%nthPrime == 0) {
                     return false;
                 }
                 nthPrime = getNthPrime(n++);
@@ -35,21 +55,19 @@ public class Tools {
             return true;
         }
     }
-    BigInteger getNthTriangleNumber(BigInteger n){
-        return n.multiply(n.add(BigInteger.ONE)).divide(BigInteger.valueOf(2));
-    }
-    long getDivisorCount(BigInteger n){
-        int primeIndex=1;
-        BigInteger currentPrime=getNthPrime(primeIndex++);
-        long divisorCount=0;
-        while(currentPrime.compareTo(n)==-1){
-            int currentPrimeDivCount=0;
-            while(n.mod(currentPrime).compareTo(BigInteger.ZERO)==0){
-                currentPrimeDivCount++;
+    List<Long> primeNumbers = new ArrayList<>();
+
+    long getNthPrime(int n) {
+        long biggestPrime = primeNumbers.get(primeNumbers.size() - 1);
+        long candidate=biggestPrime+1;
+        while (primeNumbers.size() < n) {
+
+            if (isPrime(candidate)) {
+                primeNumbers.add(candidate);
             }
-            divisorCount+=currentPrimeDivCount;
-            currentPrime=getNthPrime(primeIndex++);
+            candidate +=1;
         }
-        return  divisorCount;
+        return primeNumbers.get(n-1);
+
     }
 }
